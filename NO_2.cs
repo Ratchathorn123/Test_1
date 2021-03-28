1880625b9da4760df2d793ae421b795c67924ac5
Ratchathorn123 <79511320+Ratchathorn123@users.noreply.github.com>
2021-03-28 21:10:18 +0700

    Add files via upload

---
+++ NO_2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Test_NO._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int x = 0;
+            int y = 0;
+            x = a;
+            y = b;
+
+            while (x != y)
+
+            {
+                if (x < y)
+                {
+                    x = x + a;
+
+                }
+                else
+                {
+                    y = y + b;
+                }
+            }
+            Console.Write("{0}", x);
+        }
+    }
+}
